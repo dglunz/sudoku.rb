4f588566d7637c84f75ef9c85f97ce055cf8901b
Daniel Glunz <dannyglunz@gmail.com>
2015-01-19 18:39:50 -0700

    functional grid with all square objects and unit lists

--- sudoku.rb
+++ sudoku.rb
@@ -13,7 +13,11 @@ class Grid
   end
 
   def grid_layout
-    @rows.flat_map { |x| @cols.map { |y| [x,y] } }
+    combine(@rows, @cols)
+  end
+
+  def combine(rows, cols)
+    rows.flat_map { |x| cols.map { |y| [x,y] } }
   end
 
   def create_squares
@@ -25,15 +29,38 @@ class Grid
       square.value = puzzle[index] if puzzle[index] != 0
     end
   end
+
+  def vertical_unit(selected)
+    squares.select { |square| square.location[0] == selected.location[0] }
+  end
+
+  def horizontal_unit(selected)
+    squares.select { |square| square.location[1] == selected.location[1] }
+  end
+
+  def block_unit(selected)
+    block_rows = [['A', 'B', 'C'],['D', 'E', 'F'],['G', 'H', 'I']]
+    block_cols = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
+    row = block_rows.index { |br| br.include? selected.location[0] }
+    col = block_cols.index { |bc| bc.include? selected.location[1] }
+    block = combine(block_rows[row], block_cols[col])
+    squares.select { |square| block.include? square.location }
+  end
+
+  def unit_lists
+    { v: vertical_unit, h: horizontal_unit, b: block_unit }
+  end
 end
 
 class Square
+  attr_reader :location
   attr_accessor :value
 
   def initialize(location, value=0)
     @location = location
     @value    = value
   end
+
 end
 
 TEST_GRID = "
@@ -49,5 +76,4 @@ TEST_GRID = "
           005010300
 "
 a = Grid.new(TEST_GRID)
-#puts a.squares
-#a.squares.each { |s| require 'pry'; binding.pry;  puts s.value }
+puts a.block_unit(a.squares.last)
